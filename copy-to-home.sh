8df767a79a45bc4cb84e6ead5189607e424410fb
Reuben Thomas-Davis <reubenestd@gmail.com>
2019-12-19 11:45:01 +0000

    :sparkles: Add check to prevent alias conflicts

--- copy-to-home.sh
+++ copy-to-home.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
+function checkconflicts () {
+    cat ./.aliases.sh | sed 's/^[^# ]* *\|=.*$//g' | while read a; do
+    printf "%20.20s : %s\n" $a "$(type -ta $a | tr '\n' ' ')"
+    done | awk -F: '$2 ~ /file/'
+}
+if [[ $(checkconflicts) ]]; then
+    echo "Conflicts found see above, modify alias first"
+    exit 1
+else
+    echo "No conflicts found, safe to go ahead"
+fi
 cp .aliases.sh ~/.aliases
 cp .functions.sh ~/.functions
 cat ~/.bashrc | grep source | grep .aliases || echo "source ~/.aliases" >> ~/.bashrc
