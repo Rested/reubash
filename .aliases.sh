3c2824c581c45000806900e7f5925704bdc2eb59
Reuben Thomas-Davis <reubenestd@gmail.com>
2019-12-19 11:18:43 +0000

    :sparkles: Add additional aliases

--- .aliases.sh
+++ .aliases.sh
@@ -4,15 +4,33 @@
 alias k='kubectl'
 alias kg='kubectl get'
 alias kgp='kubectl get pods'
+alias kdep='kubectl describe pod'
+alias ka='kubectl apply'
+alias kaf='kubectl apply -f'
+alias kl='kubectl logs'
+alias klf='kubectl logs -f'
+alias kgn='kubectl get nodes'
+## other kubernetes stuff
 alias h='helm'
 
 # docker
 alias dc='docker-compose'
 alias d='docker'
+alias dbt='docker build .'
+alias dr='docker run'
 
 # git
 alias grao='git remote add origin'
-
+alias g='git'
+alias gs='git status'
+alias gm='gitmoji'
+alias gd='git diff'
+alias gp='git push'
+alias ga='git add'
+alias gaa='git add .'
+alias gt='git tag'
+alias gpo='git push origin'
+alias gpsup='git push --set-upstream origin master'
 # python
 alias der='dotenv run'
 alias podo='poetry run der'
@@ -21,3 +39,5 @@ alias podo='poetry run der'
 alias t='touch'
 alias sp='source /home/r/.bashrc'
 alias x='chmod +x'
+alias sa='source /home/r/.aliases'
+
